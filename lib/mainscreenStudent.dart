250be50728884e21074ea3c76bc6afde0813220a
Mohamed Saadi <s125317@ap.be>
2022-04-06 16:54:19 +0200

    Initial commit

---
+++ lib/mainscreenStudent.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MainscreenStudent extends StatefulWidget {
+  const MainscreenStudent({Key? key}) : super(key: key);
+
+  @override
+  State<MainscreenStudent> createState() => _MainscreenStudentState();
+}
+
+class _MainscreenStudentState extends State<MainscreenStudent>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'You are logged in as a student',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
