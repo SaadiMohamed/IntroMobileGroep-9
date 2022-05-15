059160b942a602823f14efdf18036b740be2dfd6
Mohammed <mohammedsouleymane@outlook.com>
2022-05-15 13:39:27 +0200

    mark open questions

--- lib/make_exam.dart
+++ lib/make_exam.dart
@@ -13,6 +13,14 @@ class Makeexam extends StatefulWidget {
 }
 
 class _MakeexamState extends State<Makeexam> {
+  var options = [
+    Text(""),
+    OpenQuestion(),
+    MultipleChoice(),
+    CodeCorrection(),
+    SetTime()
+  ];
+
   @override
   initState() {
     super.initState();
@@ -25,11 +33,25 @@ class _MakeexamState extends State<Makeexam> {
     dynamic result = await getquestions();
     if (result != null) {
       setState(() {
-        questions = result;
+        questions = result[0];
+        takenexam = result[1];
       });
     } else {
       print("unable to retrieve");
     }
+  }
+
+  List takenexam = [];
+  Future takenexamen() async {
+    var collection = FirebaseFirestore.instance.collection("taken");
+    var querySnapshot = await collection.get();
+    var result = [];
+    for (var queryDocumentSnapshot in querySnapshot.docs) {
+      Map<String, dynamic> data = queryDocumentSnapshot.data();
+      data['id'] = queryDocumentSnapshot.id;
+      result.add(data);
+    }
+    return result;
   }
 
   Future getquestions() async {
@@ -41,136 +63,114 @@ class _MakeexamState extends State<Makeexam> {
       data['id'] = queryDocumentSnapshot.id;
       result.add(data);
     }
-    return result;
+
+    var collection2 = FirebaseFirestore.instance.collection("taken");
+    var querySnapshot2 = await collection2.get();
+    var result2 = [];
+    for (var queryDocumentSnapshot in querySnapshot2.docs) {
+      Map<String, dynamic> data = queryDocumentSnapshot.data();
+      data['id'] = queryDocumentSnapshot.id;
+      result2.add(data);
+    }
+    return [result, result2];
   }
 
+  Widget main(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Make Exam"),
+      ),
+      floatingActionButton: questions.isEmpty
+          ? null
+          : ElevatedButton(
+              child: const Text("Delete all"),
+              onPressed: () {
+                var collection =
+                    FirebaseFirestore.instance.collection('questions');
+
+                for (var item in questions) {
+                  collection.doc(item['id']).delete();
+                }
+
+                var done = FirebaseFirestore.instance.collection('done');
+                var taken = FirebaseFirestore.instance.collection('taken');
+                Map<String, dynamic> all = {};
+                for (var item in takenexam) {
+                  all[item['id']] = {
+                    'firstname': item['firstname'],
+                    'lastname': item['lastname'],
+                    'score': item['score'],
+                    'total': item['total'],
+                  };
+                  taken.doc(item['id']).delete();
+                }
+                if (all.length > 0) {
+                  done.add(all);
+                }
+
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Makeexam()));
+              }),
+      body: ListView.builder(
+          itemCount: questions.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                title: Text("Question: " + questions[index]["question"]),
+                subtitle: questions[index]["type"] != 'opn'
+                    ? Text("Answer: " + questions[index]["answer"])
+                    : const Text("This is an open question"),
+              ),
+            );
+          }),
+    );
+  }
+
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.blue,
+          backgroundColor: Colors.blue,
+          currentIndex: _currentIndex,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: "View questions",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.question_answer),
+              label: 'Open question',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.question_answer),
+              label: "Multiple choice",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.code),
+              label: "Code correction",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.timer),
+              label: "Set time",
+            ),
+          ],
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ), // ,
         body: FutureBuilder(
             future: getquestions(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.done) {
-                return Scaffold(
-                  appBar: AppBar(
-                    title: const Text('Kies soort vraag'),
-                  ),
-                  body: ListView.builder(
-                      itemCount: questions.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: ListTile(
-                            title: Text(
-                                "Question: " + questions[index]["question"]),
-                            subtitle: questions[index]["type"] != 'opn'
-                                ? Text("Answer: " + questions[index]["answer"])
-                                : Text("This is an open question"),
-                          ),
-                        );
-                      }),
-                  floatingActionButton: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: (Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          mainAxisSize: MainAxisSize.max,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  primary: Colors.blue,
-                                  onPrimary: Colors.white,
-                                  shadowColor: Colors.greenAccent,
-                                  elevation: 3,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(32.0)),
-                                  minimumSize: const Size(200, 80),
-                                  maximumSize: const Size(200, 80) //////// HERE
-                                  ),
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            MultipleChoice()));
-                              },
-                              child: const Text('Multiple Choice',
-                                  style: TextStyle(fontSize: 22)),
-                            ),
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.blue,
-                                onPrimary: Colors.white,
-                                shadowColor: Colors.greenAccent,
-                                elevation: 3,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(32.0)),
-                                minimumSize: const Size(200, 80), //////// HERE
-                              ),
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => OpenQuestion()));
-                              },
-                              child: const Text('Open vraag',
-                                  style: TextStyle(fontSize: 22)),
-                            ),
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  primary: Colors.blue,
-                                  onPrimary: Colors.white,
-                                  shadowColor: Colors.greenAccent,
-                                  elevation: 3,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(32.0)),
-                                  minimumSize: const Size(200, 80),
-                                  maximumSize: const Size(200, 80) //////// HERE
-                                  ),
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            CodeCorrection()));
-                              },
-                              child: const Text(
-                                'Code correctie',
-                                style: TextStyle(fontSize: 22),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  primary: Colors.blue,
-                                  onPrimary: Colors.white,
-                                  shadowColor: Colors.greenAccent,
-                                  elevation: 3,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(32.0)),
-                                  minimumSize: const Size(200, 80),
-                                  maximumSize: const Size(200, 80) //////// HERE
-                                  ),
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => SetTime()));
-                              },
-                              child: const Text(
-                                'Set Time',
-                                style: TextStyle(fontSize: 22),
-                                textAlign: TextAlign.center,
-                              ),
-                            )
-                          ],
-                        ),
-                      ))),
-                );
+                if (_currentIndex == 0) {
+                  return main(context);
+                }
+                return options[_currentIndex];
               }
               return const Center(child: CircularProgressIndicator());
             }));
--- lib/open_vraag_verbeter.dart
+++ lib/open_vraag_verbeter.dart
@@ -1,20 +1,21 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:project/correct_exam.dart';
+import 'package:project/mark_open_question.dart';
+import 'package:project/view_students.dart';
 
 class OpenVraagVerbeter extends StatefulWidget {
-  OpenVraagVerbeter({Key? key, required this.questions}) : super(key: key);
-  var questions;
+  OpenVraagVerbeter({Key? key, required this.information}) : super(key: key);
+  var information;
 
   @override
-  State<StatefulWidget> createState() => _OpenVraagVerbeter(questions);
+  State<StatefulWidget> createState() => _OpenVraagVerbeter(information);
 }
 
 class _OpenVraagVerbeter extends State<OpenVraagVerbeter> {
-  _OpenVraagVerbeter(this.questions);
-  var questions;
+  _OpenVraagVerbeter(this.information);
+  var information;
 
   @override
   Widget build(BuildContext context) {
@@ -22,16 +23,52 @@ class _OpenVraagVerbeter extends State<OpenVraagVerbeter> {
         appBar: AppBar(
           title: const Text("Verbeter open vragen"),
         ),
-        body: ListView.builder(
-            itemCount: questions.length,
-            itemBuilder: (context, index) {
-              return Card(
-                child: ListTile(
-                    onTap: () {
-                    },
-                    title: Text(questions[index]["question"]),
-                    subtitle: Text("answer: " + questions[index]["studentAnswer"], style: const TextStyle(fontSize: 15),) ));},
-        )
-    );
+        floatingActionButton: ElevatedButton(
+          onPressed: () async {
+            var collection = FirebaseFirestore.instance.collection('taken');
+            int score = int.parse(information["score"].toString());
+            int total = int.parse(information["total"].toString());
+            for (var item in information["opns"]) {
+              score += int.parse(item["score"].toString());
+              total += int.parse(item["max"].toString());
             }
+            collection.doc(information["snummer"]).update({'score': score});
+            collection.doc(information["snummer"]).update({'total': total});
+            collection.doc(information["snummer"]).update({'done': true});
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const ViewStudents(key: null)));
+          },
+          child: const Text("Indienen"),
+        ),
+        body: ListView.builder(
+          itemCount: information["opns"].length,
+          itemBuilder: (context, index) {
+            return Card(
+                child: ListTile(
+                    trailing: information["opns"][index]["max"] != 0
+                        ? const Icon(Icons.check)
+                        : const Icon(Icons.close),
+                    iconColor: information["opns"][index]["max"] != 0
+                        ? Colors.green
+                        : Colors.red,
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MarkOpenQuestion(
+                              information: information,
+                              index: index,
+                            ),
+                          ));
+                    },
+                    title: Text(information["opns"][index]["question"]),
+                    subtitle: Text(
+                      "answer: " + information["opns"][index]["studentAnswer"],
+                      style: const TextStyle(fontSize: 15),
+                    )));
+          },
+        ));
   }
+}
